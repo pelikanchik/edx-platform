2708ccb984e6b9a6d7ee4281896af006be6d740a
Valera Rozuvan <valera@edx.org>
2013-06-18 14:39:39 +0300

    Rework of adding button to Studio edit dialog. Now general to any xmodule.

--- common/lib/xmodule/xmodule/videoalpha_module.py
+++ common/lib/xmodule/xmodule/videoalpha_module.py
@@ -165,4 +165,5 @@ class VideoAlphaDescriptor(VideoAlphaFields, RawDescriptor):
     """Descriptor for `VideoAlphaModule`."""
     module_class = VideoAlphaModule
     template_dir_name = "videoalpha"
-    mako_template = "widgets/html-subtitles.html"
+    # mako_template = "widgets/html-subtitles.html"
+    tabs = {'Subtitles': "widgets/videoalpha/subtitles.html"}
